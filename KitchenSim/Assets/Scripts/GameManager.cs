a3789f78fbcd9df99bdd5c4b27665d8dcb7006ba
starishsky <117216668+starishsky@users.noreply.github.com>
2023-09-26 20:57:38 -0400

    main menu and buttons whee

--- KitchenSim/Assets/Scripts/GameManager.cs
+++ KitchenSim/Assets/Scripts/GameManager.cs
@@ -13,6 +13,14 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0) == true)
+        {
+            GetComponent<AudioSource>().Play();
+        }
+
+        if(Input.GetMouseButtonUp(0) == true)
+        {
+            GetComponent<AudioSource>().Stop();
+        }
     }
 }
